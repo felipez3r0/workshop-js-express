a77743f01e09d4bee7dce7b3e2987495be17731d
Felipe do E. Santo <felipez3r0@gmail.com>
2024-05-04 08:20:32 -0300

    feat: adiciona validação de campos no create

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,20 @@
 import User from '../models/user.model.js'
+import { validationResult } from 'express-validator'
 
 export default class UserController{
   static async index(req, res) {
     const users = await User.findMany()
     res.json(users)
   }
+  
   static async create(req, res) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
     const user = await User.create({
       data: req.body
     })
     res.json(user)
-  }  
+  } 
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import UserController from '../controllers/user.controller.js'
+import { createUserValidator } from '../validators/user.validator.js'
 
 const router = Router()
 
 router.get('/', UserController.index)
-router.post('/', UserController.create)
+router.post('/', createUserValidator, UserController.create)
 
 export default router
